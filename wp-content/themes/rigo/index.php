600c6692a46a8aea04a3c71bda68c4f10e0b3ab0
Hernán García <hernanjkd@gmail.com>
2020-04-10 02:32:15 +0000

    date added

--- wp-content/themes/rigo/index.php
+++ wp-content/themes/rigo/index.php
@@ -10,7 +10,7 @@ $args = wpas_get_view_data();
     </head>
     <body>
         <p style="text-align: center;"><img style="max-height: 100px;" src="https://ucarecdn.com/f2e1f61a-efc3-4bd2-aca0-a86ed35cff8d/rigobertorocket.png" /></p>
-        <h1>Hello from <?php echo $args['name']; ?>!</h1>
+        <h1>Hello from <?php echo $args['name']; ?>!</h1><small><?php echo $args['date']; ?></small>
         <p>If you are able to see this, it means the MVC pattern is working correctly.</p>
         <p>Now you have to start building your <a href="https://github.com/alesanchezr/wpas-wordpress-dash/blob/master/src/WPAS/Controller/API.md">API endpoints</a> and <a href="https://github.com/alesanchezr/wpas-wordpress-dash/tree/master/src/WPAS/Types">Entities (Custom Post Types)</a>.</p>
         <p>Here are some useful links:</p>
