82083d3a30e2e863656b3b8f7c37092aa413f352
Hernán García <hernanjkd@gmail.com>
2020-04-10 03:04:14 +0000

    php

--- wp-content/themes/rigo/src/php/Types/Car.php
+++ wp-content/themes/rigo/src/php/Types/Car.php
@@ -8,12 +8,16 @@ class Car extends BasePostType{
     public static function serialize($object){
         
         $arrayObject = (array) $object;
-        $arrayObject['make'] = get_field( 'make', $object->ID );
-        $arrayObject['model'] = get_field( 'model', $object->ID );
-        $arrayObject['year'] = get_field( 'year', $object->ID );
-        $arrayObject['color'] = get_field( 'color', $object->ID );
-
+        // $arrayObject['make'] = get_field( 'make', $object->ID );
+        // $arrayObject['model'] = get_field( 'model', $object->ID );
+        // $arrayObject['year'] = get_field( 'year', $object->ID );
+        $arr = [];
+        $arr['color'] = get_field( 'color', $object->ID );
+        $arr['id'] = $arrayObject['ID'];
+        return $arr;
         return $arrayObject;
+        
+        // print_r($object); die();
     }
 }
 
