7d7bd7caa5702e3dad25b69ca7d4e7e3bd981315
Hernán García <hernanjkd@gmail.com>
2020-04-11 03:27:24 +0000

    testing

--- wp-content/themes/rigo/src/php/Controller/SampleController.php
+++ wp-content/themes/rigo/src/php/Controller/SampleController.php
@@ -28,17 +28,26 @@ class SampleController{
     }
     
     public function addCar() {
-        $post_id = wp_insert_post([
-            'post_author' => '1', 
-            'post_type'=>'car',
-            'post_title' => 'title',
-            'post_status' => 'publish',
-            'Make'=>'alfa',
-            'Model'=>'pointer',
-            'Year'=>'1950',
-            'Color'=>'forest'
-        ]);
-        return get_post($post_id);
+
+        // $post_id = wp_insert_post([
+        //     'post_author' => '1', 
+        //     'post_type'=>'car',
+        //     'post_title' => 'title',
+        //     'post_status' => 'publish',
+        // ]);
+        $post_id = 29;
+        // update_post_meta( $post_id, 'make', 'alfa' );
+        // update_post_meta( $post_id, 'model', 'pointer' );
+        // update_post_meta( $post_id, 'year', '1950' );
+        // update_post_meta( $post_id, 'color', 'forest' );
+        
+        $post = get_post($post_id);
+        $post['make'] = get_field( 'make', $post_id );
+        $post['model'] = get_field( 'make', $post_id );
+        $post['year'] = get_field( 'make', $post_id );
+        $post['color'] = get_field( 'make', $post_id );
+        
+        return $post;
         // return WP_REST_Request::get_body();
     }
 }
