b4f1d0280beb29021f1f9e2c32ba5f133d17723c
Hernán García <hernanjkd@gmail.com>
2020-04-11 03:31:02 +0000

    posting and reponse

--- wp-content/themes/rigo/src/php/Controller/SampleController.php
+++ wp-content/themes/rigo/src/php/Controller/SampleController.php
@@ -29,23 +29,22 @@ class SampleController{
     
     public function addCar() {
 
-        // $post_id = wp_insert_post([
-        //     'post_author' => '1', 
-        //     'post_type'=>'car',
-        //     'post_title' => 'title',
-        //     'post_status' => 'publish',
-        // ]);
-        $post_id = 29;
-        // update_post_meta( $post_id, 'make', 'alfa' );
-        // update_post_meta( $post_id, 'model', 'pointer' );
-        // update_post_meta( $post_id, 'year', '1950' );
-        // update_post_meta( $post_id, 'color', 'forest' );
+        $post_id = wp_insert_post([
+            'post_author' => '1', 
+            'post_type'=>'car',
+            'post_title' => 'title',
+            'post_status' => 'publish',
+        ]);
+        update_post_meta( $post_id, 'make', 'alfa' );
+        update_post_meta( $post_id, 'model', 'pointer' );
+        update_post_meta( $post_id, 'year', '1950' );
+        update_post_meta( $post_id, 'color', 'forest' );
         
-        $post = get_post($post_id);
+        $post = (array) get_post($post_id);
         $post['make'] = get_field( 'make', $post_id );
-        $post['model'] = get_field( 'make', $post_id );
-        $post['year'] = get_field( 'make', $post_id );
-        $post['color'] = get_field( 'make', $post_id );
+        $post['model'] = get_field( 'model', $post_id );
+        $post['year'] = get_field( 'year', $post_id );
+        $post['color'] = get_field( 'color', $post_id );
         
         return $post;
         // return WP_REST_Request::get_body();
